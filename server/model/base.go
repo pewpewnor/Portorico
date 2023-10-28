7750319c1a18d2f397a0aa4ef27a0f6675156181
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-10-29 00:25:20 +0700

    feat: add user model

---
+++ server/model/base.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Base struct {
+	ID        uuid.UUID      `gorm:"type:uuid;primaryKey" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+}
+
+func (b *Base) BeforeCreate(tx *gorm.DB) error {
+	b.ID = uuid.New()
+	return nil
+}
