dacc53abee5db04d4ca4ba9bae7d7f2607dae53f
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-10-30 00:05:04 +0700

    feat: add routine to clean up soft deleted

--- server/main.go
+++ server/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"context"
 	"os"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/joho/godotenv"
 	"github.com/pewpewnor/portorico/server/handlers"
 	"github.com/pewpewnor/portorico/server/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+func cleanSoftDeleted(ctx context.Context, db *gorm.DB) {
+	ticker := time.NewTicker(10 * time.Minute)
+
+	for {
+		select {
+		case <-ticker.C:
+			for _, model := range model.Models {
+				if err := db.Unscoped().Where("deleted_at IS NOT NULL").Delete(model).Error; err != nil {
+					log.Errorf("Cannot clean up soft deleted from a model: %v", err)
+				}
+			}
+		case <-ctx.Done():
+			ticker.Stop()
+			return
+		}
+	}
+}
 
 func main() {
 	if err := godotenv.Load(".env.local"); err != nil {
@@ -44,13 +65,18 @@ func main() {
 
 	h := handlers.Handler{DB: db, Validator: validator.New()}
 
-	// app.Get("/metrics", monitor.New())
+	app.Get("/metrics", monitor.New())
 	app.Get("/statusz", h.ServerStatus)
 	app.Get("/users", h.GetAllUsers)
 	app.Post("/user", h.CreateUser)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go cleanSoftDeleted(ctx, db)
 
 	log.Infof("Starting server on port %v...", port)
 	if err := app.ListenTLS(":"+port, "server.crt", "server.key"); err != nil {
 		log.Fatalf("Cannot start server on port %v: %v\n", port, err)
 	}
+
 }
