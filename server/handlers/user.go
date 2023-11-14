c0562a93b098c69ab0cb0662930239b2abe90c1d
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-11-14 16:38:18 +0700

    chore: standardize log information message

--- server/handlers/user.go
+++ server/handlers/user.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/charmbracelet/log"
 	"github.com/gofiber/fiber/v2"
 	"github.com/pewpewnor/portorico/server/model/response"
 )
@@ -11,7 +10,7 @@ import (
 func (h *handler) GetAllUsers(c *fiber.Ctx) error {
 	users, err := h.UserRepository.GetAll()
 	if err != nil {
-		log.Errorf("Server cannot get all users: %v\n", err)
+		return c.Status(http.StatusInternalServerError).JSON(response.InternalProblem("server cannot get all users"))
 	}
 
 	return c.Status(http.StatusOK).JSON(response.Success("successfully found all users", users))
@@ -27,11 +26,10 @@ func (h *handler) CreateUser(c *fiber.Ctx) error {
 		return res
 	}
 
-	user, err := h.UserRepository.Create(body.Username, body.Password, body.Name)
+	user, err := h.UserRepository.Create(body.Username, body.Password)
 	if err != nil {
-		log.Warnf("Server cannot create user: %v\n", err)
 		return c.Status(http.StatusInternalServerError).JSON(response.InternalProblem("server cannot create user"))
 	}
 
-	return c.Status(http.StatusOK).JSON(response.Success("Successfully created user", user))
+	return c.Status(http.StatusOK).JSON(response.Success("successfully created user", user))
 }
