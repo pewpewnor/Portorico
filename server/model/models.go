76f3e33a021e08fdde964d564f736358f2eb6428
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-11-16 15:34:08 +0700

    feat: add password hashing

--- server/model/models.go
+++ server/model/models.go
@@ -1,11 +1,19 @@
 package model
 
+import "github.com/google/uuid"
+
 var Models = []any{
 	&User{},
 }
 
+type Session struct {
+	UserID uuid.UUID `gorm:"type:uuid"`
+	Token  string
+}
+
 type User struct {
 	Base
-	Username string `gorm:"uniqueIndex" json:"username"`
-	Password string `json:"password"`
+	Username string `gorm:"uniqueIndex"`
+	Password string
+	Sessions []Session
 }
