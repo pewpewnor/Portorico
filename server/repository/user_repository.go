d4564756535ed2f1301808b9744613a21e161693
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-11-16 16:24:30 +0700

    feat: add login and sessions

--- server/repository/user_repository.go
+++ server/repository/user_repository.go
@@ -11,20 +11,61 @@ type UserRepository struct {
 	DB *gorm.DB
 }
 
-func (r *UserRepository) Create(username string, password string) (model.User, error) {
+func (r *UserRepository) createSession(user model.User) (model.Session, error) {
+	token, err := utils.GenerateRandomString(32)
+	if err != nil {
+		log.Errorf("server cannot generate token when creating session: %v", err)
+		return model.Session{}, err
+	}
+
+	session := model.Session{UserID: user.ID, Token: token, User: user}
+	if err = r.DB.Create(&session).Error; err != nil {
+		log.Errorf("server cannot create session: %v", err)
+		return model.Session{}, err
+	}
+
+	return session, nil
+}
+
+func (r *UserRepository) Login(username string, password string) (model.Session, bool, error) {
+	var user model.User
+	result := r.DB.Preload("sessiion").Where("username = ?", username).Limit(1).Find(&user)
+	if err := result.Error; err != nil {
+		log.Errorf("server cannot find user: %v", err)
+		return model.Session{}, false, err
+	}
+
+	if result.RowsAffected == 0 || utils.VerifySamePassword(password, user.Password) {
+		return model.Session{}, false, nil
+	}
+
+	session, err := r.createSession(user)
+	if err != nil {
+		return model.Session{}, false, err
+	}
+
+	return session, true, nil
+}
+
+func (r *UserRepository) Create(username string, password string) (model.Session, error) {
 	hashedPassword, err := utils.EncryptPassword(password)
 	if err != nil {
 		log.Errorf("server cannot hash password when creating user: %v", err)
+		return model.Session{}, err
 	}
 
 	user := model.User{Username: username, Password: hashedPassword}
-	err = r.DB.Create(&user).Error
-	if err != nil {
+	if err = r.DB.Create(&user).Error; err != nil {
 		log.Errorf("server cannot create user: %v", err)
-		return model.User{}, err
+		return model.Session{}, err
 	}
 
-	return user, nil
+	session, err := r.createSession(user)
+	if err != nil {
+		return model.Session{}, err
+	}
+
+	return session, nil
 }
 
 func (r *UserRepository) GetAll() ([]model.User, error) {
