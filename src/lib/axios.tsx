9f9ed16333aba1b6e126945ed7939a0c68a25e56
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-11-21 01:09:53 +0700

    feat: set to store and send cookies

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -5,6 +5,7 @@ const sucessfullStatusCode = [200, 400];
 const client = axios.create({
 	baseURL: "http://localhost:8000",
 	validateStatus: (status) => sucessfullStatusCode.includes(status),
+	withCredentials: true,
 });
 
 client.defaults.headers.common["Content-Type"] = "application/json";
