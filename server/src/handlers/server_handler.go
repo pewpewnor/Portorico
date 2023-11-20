ec2d22aba40a8fd3a28ffa91d6eea2e43da0412b
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-11-21 01:09:35 +0700

    feat: add login handler

--- server/src/handlers/server_handler.go
+++ server/src/handlers/server_handler.go
@@ -6,6 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func (h *handler) ServerStatus(c *fiber.Ctx) error {
+func (h *Handler) ServerStatus(c *fiber.Ctx) error {
 	return c.Status(http.StatusOK).JSON(map[string]any{"is_healthy": true})
 }
