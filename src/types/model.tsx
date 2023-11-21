638eb2a62871b100a0659d7c3fc073fb63b28626
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-11-21 15:19:37 +0700

    feat: add model website

--- src/types/model.tsx
+++ src/types/model.tsx
@@ -1,6 +1,19 @@
-type User = {
+interface Base {
+	id: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+interface User extends Base {
 	username: string;
 	password: string;
-};
+}
 
-export type { User };
+interface Website extends Base {
+	name: string;
+	templateName: string;
+	visitorsThisMonth: number;
+	content: { [key: string]: string };
+}
+
+export type { User, Website };
